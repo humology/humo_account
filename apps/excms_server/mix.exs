41ad866e19eeed13f272b6338b9862bfa9bd0e71
Kamil Shersheyev <kamil.shersheyev@gmail.com>
2021-06-13 01:50:51 +0200

    update mix

--- apps/excms_server/mix.exs
+++ apps/excms_server/mix.exs
@@ -45,12 +45,7 @@ defmodule ExcmsServer.MixProject do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      setup: [
-        "deps.get",
-        "cmd python3 ../../deps/excms_deps/lib/copy_deps_assets.py",
-        "excms.assets.compile",
-        "cmd npm install --prefix assets"
-      ],
+      setup: ["deps.get", "excms.assets.compile", "cmd npm install --prefix assets"],
       test: ["ecto.create --quiet", "excms.ecto.migrate", "test"]
     ]
   end
