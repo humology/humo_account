576c851a65ad5011420f942c6fccbeb19b8ea479
Kamil Shersheyev <kamil.shersheyev@gmail.com>
2021-07-05 01:26:25 +0200

    update assets, deps

---
+++ config/prod_deps.exs
@@ -0,0 +1,44 @@
+use Mix.Config
+
+config :excms_account, Excms.Deps,
+  deps: [
+    :bamboo,
+    :bamboo_smtp,
+    :bcrypt_elixir,
+    :ecto_sql,
+    :gettext,
+    :jason,
+    :phoenix,
+    :phoenix_ecto,
+    :phoenix_html,
+    :phoenix_pubsub,
+    :plug_cowboy,
+    :postgrex,
+    :excms_core,
+    :excms_account
+  ]
+
+config :excms_server, Excms.Deps,
+  deps: [
+    :bamboo,
+    :bamboo_smtp,
+    :bcrypt_elixir,
+    :ecto_sql,
+    :gettext,
+    :jason,
+    :phoenix,
+    :phoenix_ecto,
+    :phoenix_html,
+    :phoenix_pubsub,
+    :plug_cowboy,
+    :postgrex,
+    :phoenix_live_dashboard,
+    :telemetry_metrics,
+    :telemetry_poller,
+    :excms_core,
+    :excms_account,
+    :excms_server
+  ]
+
+import_config "../deps/excms_core/apps/excms_core/config/config.exs"
+import_config "../apps/excms_account/config/config.exs"
