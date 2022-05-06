654592a6c42be3b79b56a5928fe24e94c2f9c138
Kamil Shersheyev <kamil.shersheyev@gmail.com>
2022-05-06 11:07:15 +0200

    update plugin_router __using__ description

--- lib/humo_account_web/plugin_router.ex
+++ lib/humo_account_web/plugin_router.ex
@@ -41,7 +41,7 @@ defmodule HumoAccountWeb.PluginRouter do
   end
 
   @doc """
-  When used, dispatch to the appropriate controller/view/etc.
+  When used, dispatch to the appropriate router.
   """
   defmacro __using__(which) when is_atom(which) do
     apply(__MODULE__, which, [])
